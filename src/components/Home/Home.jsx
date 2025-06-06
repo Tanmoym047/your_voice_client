436984bf9c327f48d4b019ddcc566faf3878603f
Tanmoym047 <tanmoym047@gmail.com>
2025-06-06 21:53:36 +0600

    newsletter and some packages installed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Slider from './Slider';
+import Newsletter from './Newsletter';
 
 const Home = () => {
     return (
         <div className='mt-6'>
             <Slider></Slider>
+            <Newsletter></Newsletter>
         </div>
     );
 };
---
+++ src/components/Home/Newsletter.jsx
@@ -0,0 +1,40 @@
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+
+
+const Newsletter = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        const { email } = data;
+        if (email) {
+            Swal.fire({
+                title: 'Thanks!',
+                text: 'Thank you for subscribing to our  newsletter',
+                icon: 'success',
+                confirmButtonText: 'OK'
+            })
+        }
+    }
+
+
+    return (
+        <div className="mt-8 flex gap-4 flex-col lg:flex-row justify-around items-center">
+            <div className='space-y-4 my-10 lg:my-12 text-center  w-full '>
+                <h2 className='text-2xl lg:text-4xl font-bold'>Subscribe to our NewsLetter</h2>
+                <p className="text-lg opacity-90">Together, we're building something truly special.</p>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)} className="join flex justify-center  w-full">
+                <input {...register('email')} className="input w-3/4 input-bordered join-item" placeholder="Email" />
+                <button className="btn bg-rose-800 text-white w-1/4 join-item rounded-r-full">Subscribe</button>
+            </form>
+        </div>
+    );
+};
+
+export default Newsletter;
