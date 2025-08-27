f25e8993588941410461de40dad488bef476b673
Tanmoym047 <tanmoym047@gmail.com>
2025-08-27 11:41:03 +0600

    some tweak

--- src/components/Home/Slider.jsx
+++ src/components/Home/Slider.jsx
@@ -48,7 +48,7 @@ const Slider = () => {
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className='bg-[url(https://plus.unsplash.com/premium_photo-1681825268400-c561bd47d586?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-[350px] lg:h-[500px] lg:bg-no-repeat bg-cover rounded-2xl flex justify-center items-center '>
+                    <div className='bg-[url(https://plus.unsplash.com/premium_photo-1681825268400-c561bd47d586?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-[350px] lg:h-[550px] lg:bg-no-repeat bg-cover rounded-2xl flex justify-center items-center '>
                         <h2 className=' text-white text-4xl  lg:text-7xl leading-relaxed lg:leading-relaxed font-poppins text-center font-bold'>With<br /> <span className='animate__animated animate__fadeInDown animate__delay-1s  text-rose-800 uppercase font-extrabold'> Like minded people</span> </h2>
                     </div>
                 </SwiperSlide>
